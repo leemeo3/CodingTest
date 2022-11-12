3df08e513e70927b95f2e8930705a7f88328e82c
leemeo3 <117142323+leemeo3@users.noreply.github.com>
2022-11-12 12:35:37 +0900

    숫자야구 미니게임

--- 20221112.java
+++ 20221112.java
@@ -6,12 +6,12 @@ public class NumberBaseball {
         int count = 10;
         // 실행 횟수
         int play = 0;
-        
+
         // 랜덤 숫자 3개 생성
         int radomNumber1 = (int) (Math.random() * 10);
         int radomNumber2 = (int) (Math.random() * 10);
         int radomNumber3 = (int) (Math.random() * 10);
-        
+
         // 카운트
         int strike = 0;
         int ball = 0;
@@ -27,12 +27,13 @@ public class NumberBaseball {
             }
             break;
         }
+        // 랜덤 숫자 중복의 경우 제거 -- 종료
 
         // 정답 출력 (속닥속닥)
         System.out.println(radomNumber1);
         System.out.println(radomNumber2);
         System.out.println(radomNumber3);
-        
+
         // 임의의 숫자 입력
         Scanner sc = new Scanner(System.in);
         int inputNumber1 = 0;
@@ -57,29 +58,30 @@ public class NumberBaseball {
                 System.out.print("세번째 숫자 : ");
                 inputNumber3 = sc.nextInt();
                 System.out.println("================================");
-                
+
                 // 10 이상의 숫자 제거
-                if (inputNumber1 >= 10 || inputNumber2 >= 10 || inputNumber3 >= 10) { 
+                if (inputNumber1 >= 10 || inputNumber2 >= 10 || inputNumber3 >= 10) {
                     System.out.println("10미만의 숫자를 입력해주세요.");
                     System.out.println((count - play) + "회 남았습니다.");
                     continue;
                 }
-                
+
                 // 0미만의 숫자 제거
-                if (inputNumber1 <= -1 || inputNumber2 <= -1 || inputNumber3 <= -1) { 
+                if (inputNumber1 <= -1 || inputNumber2 <= -1 || inputNumber3 <= -1) {
                     System.out.println("0이상의 숫자를 입력해주세요.");
                     System.out.println((count - play) + "회 남았습니다.");
                     continue;
                 }
-                
+
                 // 임의의 숫자 중복 제거
                 if (inputNumber1 == inputNumber2 || inputNumber1 == inputNumber3 || inputNumber2 == inputNumber3) {
-                    System.out.println("중복이 발견되었습니다. 다시 입력해주세요."); 
+                    System.out.println("중복이 발견되었습니다. 다시 입력해주세요.");
                     System.out.println((count - play) + "회 남았습니다.");
                     continue;
                 }
                 break;
             }
+            // 임의의 숫자 중복 제거 -- 종료
 
             // 첫번째 숫자 검사
             if (radomNumber1 == inputNumber1) {
@@ -105,22 +107,24 @@ public class NumberBaseball {
             } else {
                 out++;
             }
-            
+            // 숫자 검사 종료
+
             // 결과 출력
             System.out.print(strike + "S ");
             System.out.print(ball + "B ");
             System.out.print(out + "Out");
             System.out.println();
-            
+
             // play 카운트 1증가
             play++;
             System.out.println((count - play) + "회 남았습니다.");
-            
+
             // 실행 횟수와 총 횟수 같을시 게임 종료
             if (play == count) {
                 break;
             }
         }
+        // 게임 실행 반복 -- 종료
     }
 } 
 
